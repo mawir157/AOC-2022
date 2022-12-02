de70788004dc30c22944a3e51e8fc42a02b282b2
mawir157 <jamesmthompson2@gmail.com>
2022-12-02 09:40:18 +0000

    [02] :snowflake:

---
+++ Rust/src/day02.rs
@@ -0,0 +1,27 @@
+use crate::advent_helper::advent_helper::read_strs;
+
+pub fn score(round: &String) -> (i64, i64)
+{
+	let players: Vec<String> = round.split(" ").map(|s| s.to_string()).collect();
+	let player1:i64 = (players[0].chars().next().unwrap() as i64) - 65;
+	let player2:i64 = (players[1].chars().next().unwrap() as i64) - 88;
+
+	let result1 = (player2 - player1 + 4) % 3;
+	let final1 = 3 * result1;
+	let final2 = 3 * player2;
+	let play2 = (player1 + 3 + (player2 - 1)) % 3;
+
+	return (1 + player2 + final1, 1 + play2 + final2);
+}
+
+pub fn run()
+{
+	let rounds = read_strs("../input/input02.txt").unwrap();
+	let scores: Vec<(i64, i64)> = rounds.iter().map(|x| score(x)).collect();
+	let result: (i64, i64) = scores.iter().fold((0,0), |mut base, &x| {base.0 += x.0; base.1 += x.1; base});
+
+	println!("Day 2");
+	println!("  Part 1: {}", result.0);
+	println!("  Part 2: {}", result.1);
+	return;
+}
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,15 +1,18 @@
 mod advent_helper;
 mod day01;
+mod day02;
 
 fn main() {
 	let pattern = std::env::args().nth(1).expect("no pattern given");
 
 	match pattern.as_str()
 	{
-		"01" => day01::run(), 
+		"01" => day01::run(),
+		"02" => day02::run(), 
 		_  =>
 		{
 			day01::run();
+			day02::run();
 		},
 	}
 
