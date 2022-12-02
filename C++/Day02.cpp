46dc554ef056eeab23fd3f9c65595064c7d31fce
mawir157 <jamesmthompson2@gmail.com>
2022-12-02 08:47:35 +0000

    [01] :snowflake:

---
+++ C++/Day02.cpp
@@ -0,0 +1,37 @@
+#include "AH.h"
+
+namespace Day02
+{
+	std::pair<int, int> score(const std::string & s) {
+		const auto players = AH::Split(s, ' ');
+		const int player1 = int(players[0][0]) - 65;
+		const int player2 = int(players[1][0]) - 88;
+
+		const auto result1 = (player2 - player1 + 4) % 3;
+		const int final1 = 3*(result1);
+
+		const int final2 = 3 * player2;
+
+		int play2 = (player1 + 3 + (player2 - 1)) % 3;
+
+		return std::make_pair<int, int>(1 + player2 + final1, 1 + play2 + final2);
+	}
+
+	int Run(const std::string& filename)
+	{
+		const auto rounds = AH::ReadTextFile(filename);
+		int part1 = 0;
+		int part2 = 0;
+
+		for (auto & r : rounds) {
+			const auto p = score(r);
+			part1 += p.first;
+			part2 += p.second;
+		}
+		
+		AH::PrintSoln(1, part1, part2);
+
+		return 0;
+	}
+
+}
