3799b8a5c77ad22b413d85248b2b505681f3b7de
mawir157 <jamesmthompson2@gmail.com>
2022-12-03 16:39:37 +0000

    [03] :snowflake::snowflake:

--- Haskell/Day03.hs
+++ Haskell/Day03.hs
@@ -1,11 +1,11 @@
 import AdventHelper
 
 import Data.Char (ord)
+import Data.List.Split (chunksOf) 
 
 scoreChar :: Char -> Integer
-scoreChar c = if' ((ord c) <= 90) upper lower
-  where upper = toInteger ((ord c) - 38)
-        lower = toInteger ((ord c) - 96)
+scoreChar c = c' - (if' (c' <= 90) 38 96)
+  where c' = toInteger $ ord c
 
 score :: String -> Integer
 score s = scoreChar $ head i
@@ -13,16 +13,13 @@ score s = scoreChar $ head i
         rhs = drop (div (length s) 2) s
         i = filter (\x -> elem x lhs) rhs
 
-assignGroupBadge :: [String] -> Integer
-assignGroupBadge [e1,e2,e3] = scoreChar $ head i'
+assignBadge :: [String] -> Integer
+assignBadge [e1,e2,e3] = scoreChar $ head i'
   where i  = filter (\x -> elem x e1) e2
         i' = filter (\x -> elem x i) e3
 
-assignBadges :: [String] -> [Integer]
-assignBadges [] = []
-assignBadges s = [assignGroupBadge (take 3 s)] ++ assignBadges (drop 3 s)
-
 main = do
   f <- readFile "../input/input03.txt"
+  let l = lines f
 
-  printSoln 3 (sum $ map score $ lines f) (sum $ assignBadges $ lines f)
+  printSoln 3 (sum $ map score l) (sum . map (assignBadge) $ chunksOf 3 l)
--- Rust/src/day03.rs
+++ Rust/src/day03.rs
@@ -26,7 +26,7 @@ fn score(s: &String) -> i64
 	return score_char(c);
 }
 
-fn assign_group_badge(ss: &[String]) -> i64
+fn assign_badge(ss: &[String]) -> i64
 {
 	match &ss[..] {
 		[e1, e2, e3] => {
@@ -42,7 +42,7 @@ pub fn run()
 {
 	let bags = read_strs("../input/input03.txt").unwrap();
 	let scores: Vec<i64> = bags.iter().map(|x| score(x)).collect();
-	let part2: Vec<i64> = bags.chunks(3).map(|x| assign_group_badge(x)).collect();
+	let part2: Vec<i64> = bags.chunks(3).map(|x| assign_badge(x)).collect();
 
 	print_soln(3, scores.iter().sum::<i64>(), part2.iter().sum::<i64>());
 	return;
