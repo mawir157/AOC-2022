4f12e02e3c173634b4e024c8997bb4363b66e274
mawir157 <jamesmthompson2@gmail.com>
2022-12-04 07:44:55 +0000

    [04] :snowflake:

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -2,6 +2,7 @@ mod advent_helper;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
 	let pattern = std::env::args().nth(1).expect("no pattern given");
@@ -11,11 +12,13 @@ fn main() {
 		"01" => day01::run(),
 		"02" => day02::run(),
 		"03" => day03::run(),
+		"04" => day04::run(),
 		_ =>
 		{
 			day01::run();
 			day02::run();
 			day03::run();
+			day04::run();
 		},
 	}
 
