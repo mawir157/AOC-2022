6e538f3d31e844a68149efd4fc8e265feb39c15b
mawir157 <jamesmthompson2@gmail.com>
2022-12-04 09:50:41 +0000

    [04] :snowflake::snowflake:

--- Haskell/Day04.hs
+++ Haskell/Day04.hs
@@ -2,14 +2,8 @@ import AdventHelper
 
 import Data.List.Split (splitOn)
 
-contains :: String -> Bool
-contains s = ((l1 <= r1) && (r2 <= l2)) || ((r1 <= l1) && (l2 <= r2))
-  where [lhs,rhs] = splitOn "," s
-        [l1,l2] = map read (splitOn "-" lhs) :: [Integer]
-        [r1,r2] = map read (splitOn "-" rhs) :: [Integer]
-
-overlaps :: String -> Bool
-overlaps s = not ((l2 < r1) || (r2 < l1))
+check :: String -> (Bool, Bool)
+check s = (((l1 <= r1) && (r2 <= l2)) || ((r1 <= l1) && (l2 <= r2)), not ((l2 < r1) || (r2 < l1)))
   where [lhs,rhs] = splitOn "," s
         [l1,l2] = map read (splitOn "-" lhs) :: [Integer]
         [r1,r2] = map read (splitOn "-" rhs) :: [Integer]
@@ -17,4 +11,4 @@ overlaps s = not ((l2 < r1) || (r2 < l1))
 main = do
   f <- readFile "../input/input04.txt"
 
-  printSoln 4 (length $ filter (contains) $ lines f) (length $ filter (overlaps) $ lines f)
+  printSoln 4 (length $ filter (fst . check) $ lines f) (length $ filter (snd . check) $ lines f)
