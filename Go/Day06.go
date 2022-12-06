2699768d6b70052e165617687852832a074ae910
mawir157 <jamesmthompson2@gmail.com>
2022-12-06 07:49:08 +0000

    [06] :snowflake:

---
+++ Go/Day06.go
@@ -0,0 +1,31 @@
+package main
+
+import AH "./adventhelper"
+
+func getMarker(s string, n int) int {
+	runes := []rune(s)
+
+	for i := 0; i < len(runes); i++ {
+		p := runes[i:i+n]
+
+		hash := make(map[rune]bool)
+
+		for _, r := range p {
+			hash[r] = true
+		}
+
+		if len(hash) == n {
+			return i + n
+		}
+	}
+
+	return 0
+}
+
+func main() {
+	space, _ := AH.ReadStrFile("../input/input06.txt")
+
+	AH.PrintSoln(4, getMarker(space[0], 4), getMarker(space[0], 14))
+
+	return
+}
