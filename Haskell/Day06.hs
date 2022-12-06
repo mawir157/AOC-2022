e19d169987838a06486006762a289d7fcba0650b
mawir157 <jamesmthompson2@gmail.com>
2022-12-06 07:59:27 +0000

    [06] :snowflake::snowflake:

--- Haskell/Day06.hs
+++ Haskell/Day06.hs
@@ -4,11 +4,11 @@ import Data.List (nub)
 
 getMarker :: Int -> Int -> String -> Int
 getMarker l n s
-  | length hd == l = n + (l - 1)
+  | length hd == l = n + l
   | otherwise      = getMarker l (n + 1) (tail s)
   where hd = nub $ take l s
 
 main = do
   f <- readFile "../input/input06.txt"
 
-  printSoln 6 (getMarker 4 1 $ head $ lines f) (getMarker 14 1 $ head $ lines f)
+  printSoln 6 (getMarker 4 0 $ head $ lines f) (getMarker 14 0 $ head $ lines f)
